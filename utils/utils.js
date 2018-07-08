41423bde6eb26f4e92b7a75e681cf281a8d0fdbc
Kapil <kapillamba4@gmail.com>
2018-07-08 21:42:47 +0530

    Update Utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,7 @@
 const { JSDOM } = require('jsdom')
 const githubUrlRegex = /(https?):\/\/(www.)?(github.com\/)(.*?\/)(.*?\/)(.*?)(\/?|#[-\d\w._]+?)(\/)?$/gmi
 
+/* eslint-disable no-unused-vars */
 const updateLinksHtml = async html => {
   const dom = JSDOM.fragment(html)
   const isEmail = !!dom.querySelectorAll('.email-fragment').length
@@ -10,14 +11,20 @@ const updateLinksHtml = async html => {
       if (a && a.href && a.href === a.innerHTML && githubUrlRegex.test(a.href)) {
         try {
           const dom = JSDOM.fragment((await JSDOM.fromURL(a.href)).serialize())
-          console.log(dom.querySelectorAll('a.js-permalink-shortcut')[0].href)
-          a.href = a.innerHTML = `https://github.com${dom.querySelectorAll('a.js-permalink-shortcut')[0].href}`
-        } catch (e) {}
+          const nodeList = dom.querySelectorAll('a.js-permalink-shortcut')
+          if (nodeList.length && nodeList[0] && nodeList[0].href) {
+            a.href = a.innerHTML = `https://github.com${nodeList[0].href}`
+          }
+        } catch (e) {
+          console.log(e)
+          return html
+        }
       };
     }
   }
   return dom.firstChild.outerHTML
 }
+/* eslint-enable no-unused-vars */
 
 const updateLinksText = async text => {
   const promises = []
@@ -26,13 +33,23 @@ const updateLinksText = async text => {
     return match
   })
 
-  const replacement = await Promise.all(promises)
-  let currentIdx = 0
-  return text.replace(githubUrlRegex, (match) => {
-    const dom = JSDOM.fragment(replacement[currentIdx++].serialize())
-    console.log(dom.querySelectorAll('a.js-permalink-shortcut')[0].href);
-    return `https://github.com${dom.querySelectorAll('a.js-permalink-shortcut')[0].href}`
-  })
+  try {
+    const replacement = await Promise.all(promises)
+    let currentIdx = 0
+    return text.replace(githubUrlRegex, (match) => {
+      const dom = JSDOM.fragment(replacement[currentIdx++].serialize())
+      const nodeList = dom.querySelectorAll('a.js-permalink-shortcut')
+      if (nodeList.length && nodeList[0] && nodeList[0].href) {
+        return `https://github.com${nodeList[0].href}`
+      }
+
+      return match
+    })
+  } catch (e) {
+    console.log(e)
+  }
+
+  return text
 }
 
 module.exports = exports = {
